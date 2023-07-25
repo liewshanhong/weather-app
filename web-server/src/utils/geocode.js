ea657add2fa002a8698ae1cabe8602da7fabed50
sanabob <72040706+liewshanhong@users.noreply.github.com>
2023-07-24 22:34:24 -0700

    last update

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token='+ process.env.MAPBOX_API +'&limit=1'
-
+    console.log(url)
     request({url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
